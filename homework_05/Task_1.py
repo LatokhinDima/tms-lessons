ef648026a95025de50dc1db627a65131b199cc56
LatokhinDima <latokhindima@gmail.com>
2023-10-31 18:20:45 +0300

    ADD readme file

--- homework_05/Task_1.py
+++ homework_05/Task_1.py
@@ -1,10 +1,8 @@
 def is_year_leap(year: int) -> bool:
-    if year % 100 != 0 and (year % 400 == 0 or year % 4 == 0):
-        return True
-    else:
-        return False
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
 print(is_year_leap(2200))
 print(is_year_leap(2020))
 print(is_year_leap(800))
 print(is_year_leap(2012))
+print(is_year_leap(1700))
