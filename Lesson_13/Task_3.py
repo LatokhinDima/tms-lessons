9be2091f9696f0e88e359ff77f31c226c69009f4
LatokhinDima <latokhindima@gmail.com>
2023-11-17 21:26:32 +0300

    ADD readme file

---
+++ Lesson_13/Task_3.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+with sqlite3.connect('sqlite.db') as connection:
+    result = connection.execute('SELECT * FROM user')
+    for i in result.fetchall():
+        print(i)
+
+
+min_age = int(input('Введите минимальный возраст: '))
+result = connection.execute('SELECT * FROM user WHERE age >= ?;', (min_age,))
+for i in result.fetchall():
+    print(i)
+
+
