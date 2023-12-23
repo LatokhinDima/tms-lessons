613db24969c0cbc6e88bc47baa58ade64334324c
LatokhinDima <latokhindima@gmail.com>
2023-12-24 00:11:12 +0300

    1

--- homework_18/shop.py
+++ homework_18/shop.py
@@ -19,18 +19,27 @@ class Product:
     name: str
     description: str
     category: str
+    favorites_count: int
 
 
 def load_products() -> list[Product]:
     with sqlite3.connect(DATABASE_FILE) as connection:
         execution_result = connection.execute(
-            'SELECT id, name, description, category FROM product')
+            'SELECT id, name, description, category, favorites_count FROM product')
         return [Product(*values) for values in execution_result.fetchall()]
+
+
+def save_product(product: Product):
+    with sqlite3.connect(DATABASE_FILE) as connection:
+        data = (product.name, product.category, product.description, product.favorites_count, product.id)
+        connection.execute('UPDATE product '
+                           'SET name = ?, category = ?, description = ?, favorites_count = ?  '
+                           'WHERE id = ?', data)
 
 
 def get_product(product_id: int) -> Product:
     with sqlite3.connect(DATABASE_FILE) as connection:
-        execution_result = connection.execute('SELECT id, name, description, category '
+        execution_result = connection.execute('SELECT id, name, description, category, favorites_count '
                                               'FROM product '
                                               'WHERE id = ?', (product_id,))
         rows = execution_result.fetchall()
@@ -68,6 +77,8 @@ def product_id_detail(product_id: int):
     product = get_product(product_id)
     if product is None:
         abort(404, 'Product not found')
+    favorites_product = session.get('favorites_product', set())
+    is_favorite = product_id in favorites_product
     return f'''
     <html>
         <head>
@@ -77,11 +88,30 @@ def product_id_detail(product_id: int):
         <body>
             <p><a href="/products">Go to home page</a></p>
             <h1>{product.name}</h1>
-            <h3>Discription: {product.description} <br> Category: {product.category}</h3
+            <h3>Description: {product.description} <br> Category: {product.category}</h3
+            <form method="post" action="/product/favorite_product">
+                <input type="hidden" name="product_id" value="{product.id}"/>
+                <input type="submit" value="Add to favorite"/>
+                <span class="favorite-star">{'&#10027' if is_favorite else ""}</span>
             </form>
         </body>
     </html>
     '''
+
+
+@app.route('/product/favorite_product', methods=['POST'])
+def favorite_product():
+    product_id = int(request.form.get('product_id'))
+    product = get_product(product_id)
+    favorites_product = session.setdefault('favorites_product', set())
+    if product.id in favorites_product:
+        product.favorites_count -= 1
+        favorites_product.remove(product.id)
+    else:
+        product.favorites_count += 1
+        favorites_product.add(product.id)
+    save_product(product)
+    return redirect(f'/product/{product.id}')
 
 
 if __name__ == '__main__':
